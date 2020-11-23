e05c8c7d2f77d9b311a23babb26751c97ae57385
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-11-23 15:36:21 -0600

    switching computers

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,7 @@
-window.alert('Boop');
+var playerName = window.prompt("What is your robots name?");
+console.log(playerName);
+//this creates a function called fight
+// function fight(){
+//     window.alert("The fight has begun!")
+// };
+//fight();
